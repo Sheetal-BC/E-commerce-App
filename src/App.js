b07674a4dec6780a45ba41252a43c036123a0eb1
Sheetal BC <93135955+Sheetal-BC@users.noreply.github.com>
2022-08-02 21:46:48 +0530

    making apps error proof

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -6,14 +6,25 @@ import './App.css';
 function App() {
   const [movies,setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [retrying, setRetrying] = useState(true);
 
- 
+
+
   async function fetchMoviesHandler() {
     setIsLoading(true);
+    setError(null);
+    try{
+      const response= await fetch('https://swapi.py4e.com/api/film/')
+      if(!response.ok){
+        throw new Error('Somthing went wrong...Retrying!');
+       }
+      
+    
+      const data = await response.json();
 
-   const response= await fetch('https://swapi.py4e.com/api/films/')
-    const data = await response.json();
-      const transformedMovies = data.results.map(movieData =>{
+    
+      const transformedMovies = data.results.map((movieData) =>{
         return {
           id : movieData.episode_id,
           title : movieData.title,
@@ -22,19 +33,44 @@ function App() {
         };
       });
       setMovies(transformedMovies);
+   
+
+    } catch(error){
+      setError(error.message);
       setIsLoading(false);
-  
+
+    }
+   
+}
+const cancelRetryingHandler = ()=>{
+  if(error){
+    content = <p>{retrying && error}</p>
+    setRetrying(false);
   }
+ 
+}
+
+let content = <p>Found no movies</p>;
+
+if(movies.length > 0){
+  content = <MoviesList movies={movies} />
+}
+if(error){
+  content = <p>{retrying && error}</p>
+}
+if(isLoading){
+  content = <p>Loading...</p>
+}
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      
       <section>
-       {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-       {!isLoading && movies.length === 0 &&<p>No movies found</p>}
-       {isLoading && <p>Loading. . .</p>}
+      {content}
+      {retrying &&<button onClick={cancelRetryingHandler}>cancel</button>}
       </section>
     </React.Fragment>
   );
